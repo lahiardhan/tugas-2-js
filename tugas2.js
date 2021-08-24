11e4a9f241a347cdc3a602112534476c01aa873b
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-25 06:33:09 +0700

    Tugas 2 JS
    Tugas ini saya serahkan kepada Tim Niomic

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+let number = 4;
+let string = "tipe data string";
+let boolean = true;
+let nul = null;
+let undef;
+let array = [4, "string", false, "", -4.5];
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(nul);
+console.log(undef);
+console.log(array);
